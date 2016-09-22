820c347d601a90aafb98b430ad36ec0daab3dd41
martinmaseda <martin.maseda@gmail.com>
2016-09-22 10:55:33 +0200

    ready to eb installed as a package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='workingWithDataFrames',
+      version='0.1',
+      description='Working smart with pandas DataFrames',
+      url='https://github.com/martinmaseda/workingWithDataFrames',
+      author='Ivan Martin Maseda',
+      author_email='martin.maseda@gmail.com',
+      packages=['workingWithDataFrames'],
+      zip_safe=False)
